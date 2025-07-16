24bfe997f3f34e3cf15372a8a990b9c5596ad460
Hazem Barkallah <barkhazem@gmail.com>
2025-07-16 12:39:54 +0100

    Pagination

--- src/app/pages/past-launches/past-launches.ts
+++ src/app/pages/past-launches/past-launches.ts
@@ -4,23 +4,58 @@ import { SpacexApiService } from '../../services/spacex-api';
 import { CommonModule } from '@angular/common';
 import { PastLaunchCard } from '../../components/past-launch-card/launch-card';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-past-launches',
-  imports: [CommonModule, PastLaunchCard,FormsModule],
+  standalone: true,
+  imports: [CommonModule, PastLaunchCard, FormsModule],
   templateUrl: './past-launches.html',
-  styleUrl: './past-launches.css'
+  styleUrl: './past-launches.css',
 })
 export class PastLaunchesComponent {
   spacexApiService = inject(SpacexApiService);
+
   launches: Launch[] = [];
   filteredLaunches: Launch[] = [];
+  launchesToDisplay: Launch[] = [];
   searchName: string = '';
-  filterLaunchesByLaunchName(name: string) {
-    this.filteredLaunches= (name? this.launches.filter(launch => launch.name.includes(name)) :this.launches)
-  }
+  currentPage: number = 1;
+  totalPages: number = 1;
+
   ngOnInit() {
-    this.spacexApiService.getPastlaunches().subscribe(launches => {
+    this.spacexApiService.getPastlaunches().subscribe((launches) => {
       this.launches = launches;
-      this.filteredLaunches = launches;
-  })}
+      this.applyFiltersAndPagination();
+    });
+  }
+  onSearchChange() {
+    this.currentPage = 1;
+    this.applyFiltersAndPagination();
+  }
+
+  applyFiltersAndPagination() {
+    this.filteredLaunches = this.searchName
+      ? this.launches.filter((launch) =>
+          launch.name.toLowerCase().includes(this.searchName.toLowerCase())
+        )
+      : this.launches;
+
+    this.totalPages = Math.ceil(this.filteredLaunches.length / 10);
+    this.updateLaunchesToDisplay();
+  }
+  updateLaunchesToDisplay() {
+    this.launchesToDisplay = this.filteredLaunches.slice((this.currentPage - 1) * 10, (this.currentPage - 1) * 10 +10);
+  }
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updateLaunchesToDisplay();
+    }
+  }
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateLaunchesToDisplay();
+    }
+  }
 }
--- src/app/pages/upcoming-launches/upcoming-launches.ts
+++ src/app/pages/upcoming-launches/upcoming-launches.ts
@@ -12,16 +12,47 @@ import { FormsModule } from '@angular/forms';
 })
 export class UpcomingLaunchesComponent {
   SpacexApiService=inject(SpacexApiService);
-  launches:Launch[] = [];
+  launches: Launch[] = [];
   filteredLaunches: Launch[] = [];
+  launchesToDisplay: Launch[] = [];
   searchName: string = '';
-  filterLaunchesByLaunchName(name: string) {
-    this.filteredLaunches= (name? this.launches.filter(launch => launch.name.includes(name)) :this.launches)
-  }
+  currentPage: number = 1;
+  totalPages: number = 1;
+
   ngOnInit() {
-    this.SpacexApiService.getUpcomingLaunches().subscribe(launches=>{
+    this.SpacexApiService.getPastlaunches().subscribe((launches) => {
       this.launches = launches;
-      this.filteredLaunches = launches
-    })
-}
+      this.applyFiltersAndPagination();
+    });
+  }
+  onSearchChange() {
+    this.currentPage = 1;
+    this.applyFiltersAndPagination();
+  }
+
+  applyFiltersAndPagination() {
+    this.filteredLaunches = this.searchName
+      ? this.launches.filter((launch) =>
+          launch.name.toLowerCase().includes(this.searchName.toLowerCase())
+        )
+      : this.launches;
+
+    this.totalPages = Math.ceil(this.filteredLaunches.length / 10);
+    this.updateLaunchesToDisplay();
+  }
+  updateLaunchesToDisplay() {
+    this.launchesToDisplay = this.filteredLaunches.slice((this.currentPage - 1) * 10, (this.currentPage - 1) * 10 +10);
+  }
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updateLaunchesToDisplay();
+    }
+  }
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateLaunchesToDisplay();
+    }
+  }
 }
