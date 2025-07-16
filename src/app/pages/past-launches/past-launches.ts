2d74ed0e599c27fba4269ee43e8436c0566915bd
Hazem Barkallah <barkhazem@gmail.com>
2025-07-16 10:45:12 +0100

    SearchBar added

--- src/app/pages/past-launches/past-launches.ts
+++ src/app/pages/past-launches/past-launches.ts
@@ -3,18 +3,24 @@ import { Launch } from '../../models/launch';
 import { SpacexApiService } from '../../services/spacex-api';
 import { CommonModule } from '@angular/common';
 import { PastLaunchCard } from '../../components/past-launch-card/launch-card';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-past-launches',
-  imports: [CommonModule,PastLaunchCard],
+  imports: [CommonModule, PastLaunchCard,FormsModule],
   templateUrl: './past-launches.html',
   styleUrl: './past-launches.css'
 })
 export class PastLaunchesComponent {
   spacexApiService = inject(SpacexApiService);
   launches: Launch[] = [];
+  filteredLaunches: Launch[] = [];
+  searchName: string = '';
+  filterLaunchesByLaunchName(name: string) {
+    this.filteredLaunches= (name? this.launches.filter(launch => launch.name.includes(name)) :this.launches)
+  }
   ngOnInit() {
     this.spacexApiService.getPastlaunches().subscribe(launches => {
       this.launches = launches;
-    });
-  }
+      this.filteredLaunches = launches;
+  })}
 }
--- src/app/pages/upcoming-launches/upcoming-launches.ts
+++ src/app/pages/upcoming-launches/upcoming-launches.ts
@@ -3,18 +3,25 @@ import { Launch } from '../../models/launch';
 import { SpacexApiService } from '../../services/spacex-api';
 import { CommonModule } from '@angular/common';
 import { UpcomingLaunchCard } from '../../components/upcoming-launch-card/upcoming-launch-card';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-upcoming-launches',
-  imports: [CommonModule, UpcomingLaunchCard],
+  imports: [CommonModule, UpcomingLaunchCard,FormsModule],
   templateUrl: './upcoming-launches.html',
   styleUrl: './upcoming-launches.css'
 })
 export class UpcomingLaunchesComponent {
-  launches:Launch[] = [];
   SpacexApiService=inject(SpacexApiService);
+  launches:Launch[] = [];
+  filteredLaunches: Launch[] = [];
+  searchName: string = '';
+  filterLaunchesByLaunchName(name: string) {
+    this.filteredLaunches= (name? this.launches.filter(launch => launch.name.includes(name)) :this.launches)
+  }
   ngOnInit() {
     this.SpacexApiService.getUpcomingLaunches().subscribe(launches=>{
       this.launches = launches;
+      this.filteredLaunches = launches
     })
 }
 }
